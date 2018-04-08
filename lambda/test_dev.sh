a13c98d4429e23e894b2a4787eab6f8bf461aa4e
keith <keithjosephrozario@gmail.com>
2018-04-09 00:37:43 +0800

    Modified regression test packs

---
+++ lambda/test_dev.sh
@@ -0,0 +1,25 @@
+echo -e "Results: 200 with body (first record 1govserv.1govnet.gov.my)\n"
+sls invoke -f get_by_fqdn -p test/200_get_by_fqdn.json
+echo -e "Results: 200 with body (last recored www2.selangor.gov.my)\n"
+sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_2.json
+echo -e "Results: 200 with body (record with decimal pengundi.spr.gov.my)\n"
+sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_decimal.json
+echo -e "\n\nResults: 400 with no body\n"
+sls invoke -f get_by_fqdn -p test/400_get_by_fqdn.json
+echo -e "\n\nResults: 404 with no body\n"
+sls invoke -f get_by_fqdn -p test/404_get_by_fqdn.json
+echo -e "\n\nList Scans\n"
+sls invoke -f list_scans
+echo -r "\n\nDownload Scans\n"
+sls invoke -f download_zip -p test/200_download_zip.json >> output.txt
+echo -e "\n\nCurling Rest API\n"
+curl https://siteaudit.sayakenahack.com/api-dev/siteDetails?FQDN=www2.selangor.gov.my
+echo -e "\n\nCurling Rest API (non-https)\n"
+curl http://siteaudit.sayakenahack.com/api-dev/siteDetails?FQDN=1govserv.1govnet.gov.my
+echo -e "\n\nCurling Rest API (decimal record)\n"
+curl https://siteaudit.sayakenahack.com/api-dev/siteDetails?FQDN=pengundi.spr.gov.my
+echo -e "\n\nCurling Rest listed scans\n"
+curl https://siteaudit.sayakenahack.com/api-dev/listScans
+echo -e "\n\nGetting a sample download\n"
+wget https://siteaudit.sayakenahack.com/api-dev/downloadScan?fileName=scan_2018-04-07.zip
+echo -e "\n#####DONE####\n"
