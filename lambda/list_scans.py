39d9681731e18635018a8ebe7a5e35938028a67f
keith <keithjosephrozario@gmail.com>
2018-04-09 00:37:21 +0800

    Returns List of JSON Objects

--- lambda/list_scans.py
+++ lambda/list_scans.py
@@ -1,6 +1,7 @@
 import boto3
 import decimal
 import json
+from operator import itemgetter
 
 headers = {'Access-Control-Allow-Origin': '*'}  # allow CORS
 
@@ -8,7 +9,7 @@ headers = {'Access-Control-Allow-Origin': '*'}  # allow CORS
 def list_scans(event, context):
 
     bucket_name = 'files.siteaudit.sayakenahack.com'
-    base_url = 'https://siteaudit.sayakenahack.com/api/downloadScans?file='
+    base_url = 'https://siteaudit.sayakenahack.com/api/downloadScan?fileName='
     keys = []
     status_code = 200
     file_prefix = 'scan'
@@ -21,12 +22,15 @@ def list_scans(event, context):
             url = base_url + content['Key']
             file_size = decimal.Decimal(content['Size'] / (1024 * 1024))
             file_size_string = str(round(file_size, 2)) + ' MB'
-            keys.append([content['Key'], url, file_size_string])
+            keys.append({'fileName': content['Key'],
+                         'url': url,
+                         'fileSize': file_size_string})
 
-    # sort by name, reverse to show latest file first
-    keys.sort(reverse=True)
+    # sort by fileName, reverse to show latest file first
+    keys_sorted = sorted(keys, key=itemgetter('fileName'), reverse=True)
+
     # Json needs to be string for API Gateway
-    result = json.dumps({'files': keys})
+    result = json.dumps({'files': keys_sorted})
 
     return {'statusCode': status_code,
             'headers': headers,
