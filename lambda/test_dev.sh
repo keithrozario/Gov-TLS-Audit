f05e3e7bb37c8d6f839772c3d97932e34e222c68
keith <keithjosephrozario@gmail.com>
2018-04-13 23:24:27 +0800

    Refactored Test Scripts with Bash Variables

--- lambda/test_dev.sh
+++ lambda/test_dev.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
 APIBASEURL=https://05upeahuti.execute-api.us-west-2.amazonaws.com/Dev
+STAGE=""
 echo -e "Results: 200 with body (first record 1govserv.1govnet.gov.my)\n"
-sls invoke -f get_by_fqdn -p test/200_get_by_fqdn.json
+sls invoke -f get_by_fqdn -p test/200_get_by_fqdn.json $STAGE
 echo -e "Results: 200 with body (last recored www2.selangor.gov.my)\n"
-sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_2.json
+sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_2.json $STAGE
 echo -e "Results: 200 with body (record with decimal pengundi.spr.gov.my)\n"
-sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_decimal.json
+sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_decimal.json $STAGE
 echo -e "\n\nResults: 400 with no body\n"
-sls invoke -f get_by_fqdn -p test/400_get_by_fqdn.json
+sls invoke -f get_by_fqdn -p test/400_get_by_fqdn.json $STAGE
 echo -e "\n\nResults: 404 with no body\n"
-sls invoke -f get_by_fqdn -p test/404_get_by_fqdn.json
+sls invoke -f get_by_fqdn -p test/404_get_by_fqdn.json $STAGE
 echo -e "\n\nList Scans\n"
-sls invoke -f list_scans
+sls invoke -f list_scans $STAGE
 echo -e "\n\nCurling Rest API\n"
 curl $APIBASEURL/siteDetails?FQDN=www2.selangor.gov.my
 echo -e "\n\nCurling Rest API (non-https)\n"
@@ -19,12 +21,14 @@ echo -e "\n\nCurling Rest API (decimal record)\n"
 curl $APIBASEURL/siteDetails?FQDN=pengundi.spr.gov.my
 echo -e "\n\nCurling Rest listed scans\n"
 curl $APIBASEURL/listScans
-echo -e "\n\nGetting a sample download\n"
+echo -e "\n\nGetting a sample download via lambda\n"
 wget $APIBASEURL/downloadScan?fileName=scan_2018-04-07.zip
+echo -e "\n\nDeleting Download\n"
+rm downloadScan?fileName=scan_2018-04-07.zip
 echo -e "\n\nResults: 404 with no body\n"
-sls invoke -f history_details -p test/200_get_by_fqdn.json
+sls invoke -f history_details -p test/200_get_by_fqdn.json $STAGE
 echo -e "Results: 200 with body (record with decimal pengundi.spr.gov.my)\n"
-sls invoke -f history_details -p test/200_get_by_fqdn_decimal.json
+sls invoke -f history_details -p test/200_get_by_fqdn_decimal.json $STAGE
 echo -e "\n\nCurling History for pengundi.spr.gov.my\n"
 curl $APIBASEURL/siteHistory?FQDN=pengundi.spr.gov.my
 echo -e "\n\nCurling History for www.skmm.gov.my"
--- lambda/test_prod.sh
+++ lambda/test_prod.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
+APIBASEURL=https://api.sayakenahack.com
+SITEURL=https://gov-tls-audit.sayakenahack.com
+ZIPFILE=scan_2018-04-13.zip
+STAGE="--stage Prod"
 echo -e "Results: 200 with body (first record 1govserv.1govnet.gov.my)\n"
-sls invoke -f get_by_fqdn -p test/200_get_by_fqdn.json --stage Prod
+sls invoke -f get_by_fqdn -p test/200_get_by_fqdn.json $STAGE
 echo -e "Results: 200 with body (last recored www2.selangor.gov.my)\n"
-sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_2.json --stage Prod
+sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_2.json $STAGE
 echo -e "Results: 200 with body (record with decimal pengundi.spr.gov.my)\n"
-sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_decimal.json --stage Prod
+sls invoke -f get_by_fqdn -p test/200_get_by_fqdn_decimal.json $STAGE
 echo -e "\n\nResults: 400 with no body\n"
-sls invoke -f get_by_fqdn -p test/400_get_by_fqdn.json --stage Prod
+sls invoke -f get_by_fqdn -p test/400_get_by_fqdn.json $STAGE
 echo -e "\n\nResults: 404 with no body\n"
-sls invoke -f get_by_fqdn -p test/404_get_by_fqdn.json --stage Prod
+sls invoke -f get_by_fqdn -p test/404_get_by_fqdn.json $STAGE
 echo -e "\n\nList Scans\n"
-sls invoke -f list_scans --stage Prod
-echo -r "\n\nDownload Scans\n"
-sls invoke -f download_zip -p test/200_download_zip.json >> output.txt --stage Prod
+sls invoke -f list_scans $STAGE
 echo -e "\n\nCurling Rest API\n"
-curl https://api.sayakenahack.com/siteDetails?FQDN=www2.selangor.gov.my
+curl $APIBASEURL/siteDetails?FQDN=www2.selangor.gov.my
 echo -e "\n\nCurling Rest API (non-https)\n"
-curl https://api.sayakenahack.com/api/siteDetails?FQDN=1govserv.1govnet.gov.my
+curl $APIBASEURL/siteDetails?FQDN=1govserv.1govnet.gov.my
 echo -e "\n\nCurling Rest API (decimal record)\n"
-curl https://api.sayakenahack.com/siteDetails?FQDN=pengundi.spr.gov.my
-echo -e "\n\nCurling Rest listed scans\n"
-curl https://api.sayakenahack.com/listScans
-echo -e "\n\nGetting a sample download\n"
-wget https://api.sayakenahack.com/downloadZip?file=scan_2018-04-07.zip scan_2018-04-07.zip
+curl $APIBASEURL/siteDetails?FQDN=pengundi.spr.gov.my
+echo -e "\n\nCurling Rest listed scans\n" 
+curl $APIBASEURL/listScans
+echo -e "\n\nGetting a sample download via lambda\n"
+wget $APIBASEURL/downloadScan?fileName=$ZIPFILE
+echo -e "\n\nDeleting Download\n"
+rm downloadScan?fileName=$ZIPFILE
+echo -e "\n\nGetting a sample download via url\n"
+wget $SITEURL/files/$ZIPFILE
+echo -e "\n\nDeleting Downloaded\n"
+rm $ZIPFILE
+echo -e "\n\nSite History: \n"
+sls invoke -f history_details -p test/200_get_by_fqdn.json $STAGE
+echo -e "\n\nResults: 200 with body (record with decimal pengundi.spr.gov.my)\n"
+sls invoke -f history_details -p test/200_get_by_fqdn_decimal.json $STAGE
+echo -e "\n\nCurling History for pengundi.spr.gov.my\n"
+curl $APIBASEURL/siteHistory?FQDN=pengundi.spr.gov.my
+echo -e "\n\nCurling History for www.skmm.gov.my"
+curl $APIBASEURL/siteHistory?FQDN=www.skmm.gov.my
 echo -e "\n#####DONE####\n"
