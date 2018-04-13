d4ec4d6e57173e77e58e72e6476b685349f7df51
keith <keithjosephrozario@gmail.com>
2018-04-13 22:06:50 +0800

    Updated to list files/ directory

--- lambda/list_scans.py
+++ lambda/list_scans.py
@@ -9,22 +9,26 @@ headers = {'Access-Control-Allow-Origin': '*'}  # allow CORS
 def list_scans(event, context):
 
     bucket_name = 'files.siteaudit.sayakenahack.com'
-    base_url = 'https://api.sayakenahack.com/downloadScan?fileName='
+    file_prefix = 'files/'  # prefix to scan files from
+    base_url = 'https://gov-tls-audit.sayakenahack.com/files/'
     keys = []
     status_code = 200
-    file_prefix = 'scan'
 
     client = boto3.client('s3')
     response = client.list_objects_v2(Bucket=bucket_name, Delimiter='|')
 
     for content in response['Contents']:
-        if content['Key'][:4] == file_prefix:
-            url = base_url + content['Key']
-            file_size = decimal.Decimal(content['Size'] / (1024 * 1024))
-            file_size_string = str(round(file_size, 2)) + ' MB'
-            keys.append({'fileName': content['Key'],
-                         'url': url,
-                         'fileSize': file_size_string})
+        if content['Key'][:len(file_prefix)] == file_prefix:
+            file_name = content['Key'][len(file_prefix):]
+            if file_name == '':
+                continue  # S3 list bucket will return folder as an object (skip folder)
+            else:
+                url = base_url + file_name
+                file_size = decimal.Decimal(content['Size'] / (1024 * 1024))
+                file_size_string = str(round(file_size, 2)) + ' MB'
+                keys.append({'fileName': file_name,
+                             'url': url,
+                             'fileSize': file_size_string})
 
     # sort by fileName, reverse to show latest file first
     keys_sorted = sorted(keys, key=itemgetter('fileName'), reverse=True)
