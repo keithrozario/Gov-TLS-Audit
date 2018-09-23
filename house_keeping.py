76ee422be6ab2d1158c0ef335441bf1344fec0c7
Keith Rozario <keith@keithrozario.com>
2018-09-23 09:30:46 +0800

    Put S3 archival into housekeeping

--- house_keeping.py
+++ house_keeping.py
@@ -2,6 +2,7 @@ import boto3
 import operator
 import logging
 from datetime import datetime, timedelta, timezone
+import custom_config
 
 '''
 Script deletes all backups, except the <x> latest backups, and any backup younger than retirement_age.
@@ -22,24 +23,20 @@ console.setLevel(logging.INFO)
 logger.addHandler(console)
 
 
-retirement_age = 3  # Only delete backups older than this many days (caveat: max_backups)
-max_backups = 2  # keep at least this many backups present (regardless of age)
-table_name = 'siteAuditGovMy'
-timezone_delta = 8  # timezone (relative to UTC), can be negative
-
-upper_bound = (datetime.now(timezone(timedelta(hours=timezone_delta))) - timedelta(days=retirement_age))
+upper_bound = (datetime.now(timezone(timedelta(hours=custom_config.timezone_delta)))
+               - timedelta(days=custom_config.retirement_age))
 
 # Query API for list of backups for table
 client = boto3.client('dynamodb')
-logger.info("Querying Backups for table: %s" % table_name)
-response = client.list_backups(TableName=table_name)
+logger.info("Querying Backups for table: %s" % custom_config.dynamo_table_name)
+response = client.list_backups(TableName=custom_config.dynamo_table_name)
 logger.info("Response received, %d backups found" % len(response['BackupSummaries']))
 
 # Sort response (latest backups first)
 response['BackupSummaries'].sort(key=operator.itemgetter('BackupCreationDateTime'), reverse=True)
 
 # Loop through, but skip latest backups (from <max_backups> onwards)
-for backup in response['BackupSummaries'][max_backups:]:
+for backup in response['BackupSummaries'][custom_config.max_backups:]:
 
     # Delete the backup if it's older than retirement_age
     if backup['BackupCreationDateTime'] < upper_bound:
@@ -48,12 +45,33 @@ for backup in response['BackupSummaries'][max_backups:]:
         logger.info('Deletion Successful: %s ' % response['BackupDescription']['BackupDetails'])
 
 # Get latest list (post-deletion)
-response = client.list_backups(TableName=table_name)
+response = client.list_backups(TableName=custom_config.dynamo_table_name)
 for backup in response['BackupSummaries']:
     logger.info('Retained Backup: %s ' % backup)
 
 
+# list out scans in S3 bucket
+file_upper_bound = (datetime.now(timezone(timedelta(hours=custom_config.timezone_delta)))
+                    - timedelta(days=custom_config.file_retirement_age))
+logger.info("Retrieving list of files in S3 bucket")
+keys = []
+client = boto3.client('s3')
+file_prefix = custom_config.s3_upload_dir+
+response = client.list_objects_v2(Bucket=custom_config.bucket_name,
+                                  Prefix=file_prefix)
 
+# Sort response (latest files first)
+response['Contents'].sort(key=operator.itemgetter('LastModified'), reverse=True)
 
+# loop through only backups after max_backup:
+for content in response['Contents'][custom_config.file_max_backups:]:
+    file_name = content['Key'][len(file_prefix):]
 
+    if file_name == '':
+        continue  # S3 list bucket will return folder as an object (skip folder)
+    else:
+        if content['LastModified'] < file_upper_bound:
+            client.copy({'Bucket': custom_config.bucket_name, 'Key': content['Key']},
+                        custom_config.bucket_name, custom_config.s3_upload_dir + file_name)
+            logger.info("%s : Archived" % file_name)
 
