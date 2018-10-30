535af9c2073d8ae146e71ff7b2d11b8767bbdf34
Keith Rozario <keith@keithrozario.com>
2018-10-30 23:36:26 +0800

    Initialization on Ubuntu18.04 on DigitalOcean

--- initialize/Gov-TLS-Init.sh
+++ initialize/Gov-TLS-Init.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 DIR=Gov-TLS-Audit
+export LC_ALL=C
+apt update
+apt install python-pip
+apt install python3-pip
+apt-get install python3-venv
+pip install virtualenv
 git clone https://github.com/keithrozario/Gov-TLS-Audit.git
 mkdir $DIR/logs
 mkdir $DIR/output
-virtualenv $DIR/venv
+python3 -m venv $DIR/venv
 source $DIR/venv/bin/activate
+export LC_ALL=C
+pip install wheel
+pip install --upgrade pip
 pip install -r $DIR/requirements.txt
+deactivate
+timedatectl set-timezone Asia/Singapore
+pip install awscli --upgrade --user
+apt install awscli
+aws configure
 echo "Don't forget to modify Shodan key location in custom_config.py to ../.shodan/key.txt"
